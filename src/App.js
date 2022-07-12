f0a421f8857aa99809963cd0c10428038a34905a
Joey Cos <joeycosentino24@gmail.com>
2022-07-11 20:20:30 -0400

    added email validation to contact form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
 import About from './componenets/About';
+import Contact from './componenets/Contact';
 import Header from './componenets/Header';
 
 function App() {
+
   return (
     <div>
       <Header></Header>
       <main>
         <About></About>
+        <Contact></Contact>
       </main>
     </div>
   );
