9fc22e9170721a6d7a1485b7051cb1dcec33fce2
Joey Cos <joeycosentino24@gmail.com>
2022-07-13 22:53:43 -0400

    slight formatting

--- src/componenets/Resume/index.js
+++ src/componenets/Resume/index.js
@@ -6,14 +6,24 @@ function Resume() {
     return (
         <div className="resume">
             Download my resume<span>
-            <a href={resume} download>Here</a></span>
+            <a href={resume} download> Here</a></span>
             <div>
                 <h3>Front-End Proficiencies</h3>
-                <ul>
+                <ul className="list-wrapper">
                     <li>HTML</li>
                     <li>CSS</li>
                     <li>Front-End JavaScript</li>
                     <li>React</li>
+                </ul>
+            </div>
+            <div>
+                <h3>Back-End Proficiencies</h3>
+                <ul className="list-wrapper">
+                    <li>Node.js</li>
+                    <li>Express</li>
+                    <li>MongoDB</li>
+                    <li>SQL</li>
+                    <li>Back-End JavaScript</li>
                 </ul>
             </div>
         </div>
